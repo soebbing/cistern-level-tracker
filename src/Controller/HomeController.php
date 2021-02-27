6f892a280b2fb24cc888eb9dd2ae5f6cc4e42028
Hendrik Söbbing <hendrik@soebbing.de>
2021-02-27 20:03:20 +0100

    Fix logging, default route
    Signed-off-by: Hendrik Söbbing <hendrik@soebbing.de>

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    /**
+     * @Route("/", name="home", methods={"GET"})
+     */
+    public function __invoke(): RedirectResponse
+    {
+        return new RedirectResponse('/admin', Response::HTTP_FOUND);
+    }
+}
